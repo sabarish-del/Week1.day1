09dec255ee7ec14d922cb11b36e951ee38f5ba08
sabarish-del <56497948+sabarish-del@users.noreply.github.com>
2021-08-08 13:15:25 +0530

    Add files via upload

---
+++ Primenumber.java
@@ -0,0 +1,28 @@
+package Week1Day1;
+
+public class Primenumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=13;
+		boolean prime=false;
+		for (int i = 2; i < n ; i++) {
+			if (i%2==0) {
+
+				prime = true;
+				break;
+			}
+			
+		}
+		if(prime==!prime)
+		{
+			System.out.println(prime);
+
+	}
+		else
+		{
+			System.out.println(prime);
+		}
+
+}
+}
